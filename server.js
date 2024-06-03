422afa8ad3aeec2975f873cc57f41e8640be48bf
Dr-AddictStein <dr.addictstein@gmail.com>
2024-06-04 05:29:20 +0600

    sss

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const upload = multer({ storage });
 
 // use of middlewars
 app.use(
-  cors({ origin: [ "http://localhost:5173", "http://194.238.17.44", "http://bookedplus.com/", "https://bookedplus.com/"] })
+  cors({ origin: [ "http://localhost:5173", "http://194.238.17.44", "http://bookedplus.com", "https://bookedplus.com"] })
 );
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
